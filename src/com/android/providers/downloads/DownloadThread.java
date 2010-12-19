aosp-mirror/platform_packages_providers_downloadprovider

Use proxy for downloads.
Uses the new Proxy method getPreferredHttpHost to easily get correct
proxy.

Change-Id: If859ad03c2ed5a896c62592aa131d0e79d1b9c02

src/com/android/providers/downloads/DownloadThread.java
@@ -20,6 +20,7 @@ import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.HttpClient;
+import org.apache.http.conn.params.ConnRouteParams;
 import org.apache.http.entity.StringEntity;
 
 import android.content.ContentUris;
@@ -27,8 +28,9 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.drm.mobile1.DrmRawContent;
-import android.net.Uri;
 import android.net.http.AndroidHttpClient;
+import android.net.Proxy;
+import android.net.Uri;
 import android.os.FileUtils;
 import android.os.PowerManager;
 import android.os.Process;
@@ -180,6 +182,10 @@ public class DownloadThread extends Thread {
              */
 http_request_loop:
             while (true) {
+                // Set or unset proxy, which may have changed since last GET request.
+                // setDefaultProxy() supports null as proxy parameter.
+                ConnRouteParams.setDefaultProxy(client.getParams(),
+                        Proxy.getPreferredHttpHost(mContext, mInfo.mUri));
                 // Prepares the request and fires it.
                 HttpGet request = new HttpGet(mInfo.mUri);
 

LineageOS/android_packages_providers_DownloadProvider

Signed-off-by: Eddie Ringle <eddie.ringle@gmail.com>

src/com/android/providers/downloads/DownloadThread.java
@@ -21,6 +21,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.drm.mobile1.DrmRawContent;
 import android.net.http.AndroidHttpClient;
+import android.net.Proxy;
+import android.net.Uri;
 import android.os.FileUtils;
 import android.os.PowerManager;
 import android.os.Process;
@@ -32,6 +34,7 @@ import android.util.Pair;
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.params.ConnRouteParams;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -161,6 +164,11 @@ public class DownloadThread extends Thread {
             boolean finished = false;
             while(!finished) {
                 Log.i(Constants.TAG, "Initiating request for download " + mInfo.mId);
+                // Set or unset proxy, which may have changed since last GET request.
+                // setDefaultProxy() supports null as proxy parameter.
+                ConnRouteParams.setDefaultProxy(client.getParams(),
+                        Proxy.getPreferredHttpHost(mContext, state.mRequestUri));
+                // Prepares the request and fires it.
                 HttpGet request = new HttpGet(state.mRequestUri);
                 try {
                     executeDownload(state, client, request);