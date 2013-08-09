3effba041c8019be2cdd5da19409c45f7529bade
Danesh M <daneshm90@gmail.com>
2013-08-09 14:40:23 -0700

    DownloadManager : Add live folder
    Add support for live folders, which mirrors downloaded content
    
    Change-Id: I145a8e044d0ac9b9bd62ebd7db4c0d6a265391f7

--- src/com/android/providers/downloads/DownloadService.java
+++ src/com/android/providers/downloads/DownloadService.java
@@ -368,6 +368,10 @@ public class DownloadService extends Service {
                     PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_ONE_SHOT));
         }
 
+        if (!isActive) {
+            LiveFolderReceiver.updateFolders(this, 0);
+        }
+
         return isActive;
     }
 
@@ -413,6 +417,7 @@ public class DownloadService extends Service {
             deleteFileIfExists(info.mFileName);
         }
         mDownloads.remove(info.mId);
+        LiveFolderReceiver.updateFolders(this, 0);
     }
 
     private void deleteFileIfExists(String path) {
