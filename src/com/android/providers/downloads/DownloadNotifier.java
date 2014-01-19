addd919d97a3192c3aa16ebd3fe73abb9faa194b
Lasse Brudeskar Vikås <bvx89.at.cs@gmail.com>
2014-01-19 22:01:57 +0100

    Add Download Speeds in notification
    This commit adds the ability to show the current total download speed
    
    Screenshots: http://goo.gl/M3eRNR
    
    ps7 : remove download site, and make speed to bit/sec
    ps8 : make speed to byte/sec and cleanup
    ps9 : revert to B/sec
    ps10: fix some formatting issues
    ps13: rebased
    
    Change-Id: I801dbe61c7ee59d0c1d14d5851ad6dc3a7678499

--- src/com/android/providers/downloads/DownloadNotifier.java
+++ src/com/android/providers/downloads/DownloadNotifier.java
@@ -35,6 +35,7 @@ import android.os.SystemClock;
 import android.provider.Downloads;
 import android.text.TextUtils;
 import android.text.format.DateUtils;
+import android.text.format.Formatter;
 import android.util.Log;
 import android.util.LongSparseLongArray;
 
@@ -218,6 +219,7 @@ public class DownloadNotifier {
             // Calculate and show progress
             String remainingText = null;
             String percentText = null;
+            String speedAsSizeText = null;
             if (type == TYPE_ACTIVE) {
                 long current = 0;
                 long total = 0;
@@ -238,8 +240,26 @@ public class DownloadNotifier {
 
                     if (speed > 0) {
                         final long remainingMillis = ((total - current) * 1000) / speed;
+                        final int duration, durationResId;
+
+                        // This duplicates DateUtils.formatDuration(), but uses our
+                        // abbreviated plurals.
+                        if (remainingMillis >= DateUtils.HOUR_IN_MILLIS) {
+                            duration = (int) ((remainingMillis + 1800000)
+                                    / DateUtils.HOUR_IN_MILLIS);
+                            durationResId = R.plurals.duration_hours;
+                        } else if (remainingMillis >= DateUtils.MINUTE_IN_MILLIS) {
+                            duration = (int) ((remainingMillis + 30000)
+                                    / DateUtils.MINUTE_IN_MILLIS);
+                            durationResId = R.plurals.duration_minutes;
+                        } else {
+                            duration = (int) ((remainingMillis + 500)
+                                    / DateUtils.SECOND_IN_MILLIS);
+                            durationResId = R.plurals.duration_seconds;
+                        }
                         remainingText = res.getString(R.string.download_remaining,
-                                DateUtils.formatDuration(remainingMillis));
+                                res.getQuantityString(durationResId, duration, duration));
+                        speedAsSizeText = Formatter.formatFileSize(mContext, speed);
                     }
 
                     builder.setProgress(100, percent, false);
@@ -252,7 +272,6 @@ public class DownloadNotifier {
             final Notification notif;
             if (cluster.size() == 1) {
                 final DownloadInfo info = cluster.iterator().next();
-
                 builder.setContentTitle(getDownloadTitle(res, info));
 
                 if (type == TYPE_ACTIVE) {
@@ -261,10 +280,9 @@ public class DownloadNotifier {
                             builder.setContentText(res.getText(R.string.download_paused));
                         else
                             builder.setContentText(res.getText(R.string.download_queued));
-                    } else if (!TextUtils.isEmpty(info.mDescription)) {
-                        builder.setContentText(info.mDescription);
-                    } else {
-                        builder.setContentText(remainingText);
+                    } else if (speedAsSizeText != null) {
+                        builder.setContentText(res.getString(R.string.download_speed_text,
+                                remainingText, speedAsSizeText));
                     }
                     builder.setContentInfo(percentText);
 
@@ -298,7 +316,8 @@ public class DownloadNotifier {
                                 R.plurals.notif_summary_active, cluster.size(), cluster.size()));
                     }
                     builder.setContentText(remainingText);
-                    builder.setContentInfo(percentText);
+                    builder.setContentInfo(res.getString(R.string.download_speed_text,
+                                percentText, speedAsSizeText));
                     inboxStyle.setSummaryText(remainingText);
 
                 } else if (type == TYPE_WAITING) {
