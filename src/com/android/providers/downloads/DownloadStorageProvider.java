5776a5e408c54209cfa63ecc12842b62fc266256
mse1969 <mse1969@posteo.de>
2026-03-31 18:33:51 +0200

    Revert "DO NOT MERGE: Consolidate queryChildDocumentsXxx() implementations"
    This reverts commit d1a6862647428e9c973f4c21adc83656c5ac98f9.
    
    This patch/backport does not apply to P. The super method, from which we
    inherit/override, does not have the 'includeHiden' argument, hence we do
    not need to and also should not consolidate.
    
    Change-Id: I34888a6ce8c6b7f3370618acd6fb506c531938ce

--- src/com/android/providers/downloads/DownloadStorageProvider.java
+++ src/com/android/providers/downloads/DownloadStorageProvider.java
@@ -245,21 +245,34 @@ public class DownloadStorageProvider extends FileSystemProvider {
         }
     }
 
-    protected Cursor queryChildDocuments(String documentId, String[] projection,
-            String sortOrder, boolean includeHidden) throws FileNotFoundException {
+    @Override
+    public Cursor queryChildDocuments(String parentDocId, String[] projection, String sortOrder)
+            throws FileNotFoundException {
+        return queryChildDocuments(parentDocId, projection, sortOrder, false);
+    }
+
+    @Override
+    public Cursor queryChildDocumentsForManage(
+            String parentDocId, String[] projection, String sortOrder)
+            throws FileNotFoundException {
+        return queryChildDocuments(parentDocId, projection, sortOrder, true);
+    }
+
+    private Cursor queryChildDocuments(String parentDocId, String[] projection,
+            String sortOrder, boolean manage) throws FileNotFoundException {
 
         // Delegate to real provider
         final long token = Binder.clearCallingIdentity();
         Cursor cursor = null;
         try {
-            if (RawDocumentsHelper.isRawDocId(documentId)) {
-                return super.queryChildDocuments(documentId, projection, sortOrder);
+            if (RawDocumentsHelper.isRawDocId(parentDocId)) {
+                return super.queryChildDocuments(parentDocId, projection, sortOrder);
             }
 
-            assert (DOC_ID_ROOT.equals(documentId));
+            assert (DOC_ID_ROOT.equals(parentDocId));
             final DownloadsCursor result = new DownloadsCursor(projection,
                     getContext().getContentResolver());
-            if (includeHidden) {
+            if (manage) {
                 cursor = mDm.query(
                         new DownloadManager.Query().setOnlyIncludeVisibleInDownloadsUi(true));
             } else {
